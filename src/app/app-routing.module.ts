9aaaae8862090d8172b9b2832219716f0868df79
fran sola <fmia1992@gmail.com>
2022-02-12 19:53:01 +0100

    Paginas añadidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogPostComponent } from './pages/blog-post/blog-post.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ProductComponent } from './pages/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: PortfolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'blog-post', component: BlogPostComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { BlogPostComponent } from './pages/blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    AboutComponent,
+    ProductComponent,
+    ContactComponent,
+    BlogComponent,
+    BlogPostComponent
   ],
   imports: [
     BrowserModule,
